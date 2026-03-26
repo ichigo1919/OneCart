0a647347c0130befc4c84bd594c85417dd995e11
jashu <chamakurijashwanth@gmal.com>
2026-03-27 02:46:35 +0530

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -3,22 +3,30 @@ import dotenv from 'dotenv'
 import connectDb from './config/db.js'
 import cookieParser from 'cookie-parser'
 import authRoutes from './routes/authRoutes.js'
-dotenv.config()
-
-import cors from "cors"
 import userRoutes from './routes/userRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cartRoutes from './routes/cartRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 
+dotenv.config()
 let port = process.env.PORT || 8000
 
 let app = express()
 
+import cors from "cors";
+
+const corsOptions = {
+  origin: "https://one-cart-eeap.vercel.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(cors());
 
 
 app.use("/api/auth",authRoutes)
