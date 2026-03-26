d125cc739d0d44fec523971095f0bef8f967d61e
jashu <chamakurijashwanth@gmal.com>
2026-03-27 02:33:58 +0530

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -18,10 +18,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174","http://localhost:5175","http://localhost:5176"],
-  credentials: true
-}));
+app.use(cors());
 
 
 app.use("/api/auth",authRoutes)
