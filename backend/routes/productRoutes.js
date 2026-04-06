0ecfaa789ff61f0c09500a49d816a7952dfcda8e
jashu <chamakurijashwanth@gmal.com>
2026-04-07 05:12:32 +0530

    listProduct removed

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addProduct, listProduct, removeProduct } from '../controller/productController.js'
+import { addProduct, removeProduct } from '../controller/productController.js'
 import upload from '../middleware/multer.js'
 import adminAuth from "../middleware/adminAuth.js"
 
@@ -12,7 +12,6 @@ productRoutes.post("/addproduct",upload.fields([
     {name:"image3",maxCount:1},
     {name:"image4",maxCount:1}]),addProduct)
 
-productRoutes.get("/list", listProduct)
 productRoutes.post("/remove/:id",adminAuth,removeProduct)
 
 
